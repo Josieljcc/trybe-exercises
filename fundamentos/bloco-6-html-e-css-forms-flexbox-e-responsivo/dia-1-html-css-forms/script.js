31ba2e67adeb5163b67b4202fa65ae81d58d33f6
Josiel da Costa <josiel.jcc@hotmail.com>
2022-07-25 16:20:11 -0300

    exercicio 6.2

--- fundamentos/bloco-6-html-e-css-forms-flexbox-e-responsivo/dia-1-html-css-forms/script.js
+++ fundamentos/bloco-6-html-e-css-forms-flexbox-e-responsivo/dia-1-html-css-forms/script.js
@@ -1,5 +1,29 @@
 const submitButton = document.querySelector("#submit");
 submitButton.addEventListener("click", handleClick);
-function handleClick(event) {
-  event.preventDefault();
-}
+function handleClick(event) {}
+
+const validation = new JustValidate("#form");
+
+validation
+  .addField("#name", [
+    {
+      rule: "minLength",
+      value: 10,
+      errorMessage: "o campo deve conter no minimo 10 caracteres",
+    },
+    {
+      rule: "maxLength",
+      value: 40,
+      errorMessage: "o campo deve conter no máximo 40 caracteres",
+    },
+  ])
+  .addField("#email", [
+    {
+      rule: "required",
+      errorMessage: "Email é obrigatório",
+    },
+    {
+      rule: "email",
+      errorMessage: "Email inválido!",
+    },
+  ]);
