cb4e32a697011cc2080c255567163d09245f9112
Josiel da Costa <josiel.jcc@hotmail.com>
2022-07-27 23:36:33 -0300

    exercicio 7.1 parte II

--- fundamentos/bloco-7-introducao-a-javascript-es6-e-testes-unitarios/dia-1-javascript-es6-let-const-arrow-functions-e-template-literals/parteI/exercicio-01.js
+++ fundamentos/bloco-7-introducao-a-javascript-es6-e-testes-unitarios/dia-1-javascript-es6-let-const-arrow-functions-e-template-literals/parteI/exercicio-01.js
@@ -1,12 +1,12 @@
-function testingScope(escopo) {
+let testingScope = (escopo) => {
   if (escopo === true) {
     let ifScope = "Não devo ser utilizada fora do meu escopo (if)";
-    ifScope = ifScope + " ótimo, fui utilizada no escopo !";
+    ifScope = `${ifScope} ótimo, fui utilizada no escopo !"`;
     console.log(ifScope);
   } else {
     let elseScope = "Não devo ser utilizada fora meu escopo (else)";
     console.log(elseScope);
   }
-}
+};
 
-testingScope(false);
+testingScope(true);
---
+++ fundamentos/bloco-7-introducao-a-javascript-es6-e-testes-unitarios/dia-1-javascript-es6-let-const-arrow-functions-e-template-literals/parteII/exercicio-01.js
@@ -0,0 +1,5 @@
+function fatorial(n) {
+  return n === 0 || n === 1 ? 1 : n * fatorial(n - 1);
+}
+
+console.log(fatorial(8));
